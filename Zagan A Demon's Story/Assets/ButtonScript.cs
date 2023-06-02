c269ec164b017ebdfc0e5b71ed70c3494cd7aa23
Bradley Lauweres <bradley.lauweres@student.howest.be>
2023-06-02 13:54:12 +0200

    Redid main menu for zagan

---
+++ Zagan A Demon's Story/Assets/ButtonScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    [SerializeField]private Button ThisButton;
+
+    private void Update()
+    {
+      
+    }
+}
